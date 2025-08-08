be6b27c4bd2818ddacdb3375b604e23a0b47d72d
rubad33 <syedmaskat420@gmail.com>
2025-08-09 01:59:28 +0600

    store and car functionality

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',  # Custom context processor
+                'carts.context_processors.counter',  # Custom context processor for cart count
             ],
         },
     },
--- store/views.py
+++ store/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product,Variation
 from category.models import Category
-
-
+from carts.models import CartItem
+from carts.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 # Create your views here.
 
 
@@ -12,23 +14,56 @@ def store(request, category_slug=None):
     if(category_slug != None):
         categorys = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categorys, is_available=True)
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available = True)
+        products = Product.objects.all().filter(is_available = True).order_by('id')
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
 
   
 
     context = {
-        'products': products,
-        'product_count': product_count,
+        'products': paged_products,
+        'product_count': product_count, 
     }
     return render(request, 'store/store.html', context)
 
 def product_detail(request, category_slug, product_slug):
-    single_product = get_object_or_404(Product, category__slug=category_slug, slug=product_slug)
+        single_product = get_object_or_404(Product, category__slug=category_slug, slug=product_slug)
+    
+        
+        colors = Variation.objects.filter(product=single_product, variation_category='color', is_active=True)
+        sizes = Variation.objects.filter(product=single_product, variation_category='size', is_active=True)
+        context = {
+            'single_product': single_product,
+            'colors': colors,
+            'sizes': sizes,
+        }
+        return render(request, 'store/product_detail.html', context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(
+                Q(description__icontains=keyword) | Q(product_name__icontains=keyword)
+            )
+            product_count = products.count()
+        else:
+            products = Product.objects.all()
+            product_count = products.count()
+    else:
+        products = Product.objects.all()
+        product_count = products.count()
+
     context = {
-        'single_product': single_product,
+        'products': products,
+        'product_count': product_count,
     }
-    return render(request, 'store/product_detail.html', context)
+    return render(request, 'store/store.html', context)
