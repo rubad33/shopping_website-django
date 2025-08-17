ad318034a80fac3ca68d48eb4eeb73f82684f4c1
rubad33 <syedmaskat420@gmail.com>
2025-08-18 03:16:57 +0600

    review and rating system

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('category/<slug:category_slug>/', views.store, name='products_by_category'),
     path('category/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),  # ✅ fixed name
     path('search/', views.search, name='search'),  # ✅ added search path
+    path('submit_review/<int:product_id>/', views.submit_review, name='submit_review'),
 ]
